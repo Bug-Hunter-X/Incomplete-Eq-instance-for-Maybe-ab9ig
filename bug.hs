4429c471879a229701d5aeac135e9001db6ba650
BugHunter-X <askzenai@gmail.com>
2025-01-18 12:12:06 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+instance Eq a => Eq (Maybe a) where
+  Nothing == Nothing = True
+  (Just x) == (Just y) = x == y
+  _ == _ = False
+
+instance (Eq a, Show a) => Show (Maybe a) where
+  show Nothing = "Nothing"
+  show (Just x) = "Just " ++ show x
+
+main :: IO ()
+main = do
+  print (Nothing :: Maybe Int)
+  print (Just 5 :: Maybe Int)
+  print (Nothing == Nothing)
+  print (Just 5 == Just 5)
+  print (Just 5 == Nothing)
